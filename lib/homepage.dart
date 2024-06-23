e80a77bd88908ac01b1b59b5c3ab9f62a305e00d
Khalil <khalilhenane@hotmail.com>
2024-06-23 12:42:32 +0200

    Changed the  sdk min/targert version, so it is compatible with my  device

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'choose_meal.dart';
+// import 'package:beta_app/pages/choosemeal.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key});
@@ -22,6 +24,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
