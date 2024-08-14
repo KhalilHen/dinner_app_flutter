e563c2190fa41becc9db6a7aba03e5c31a530a53
Khalil <khalilhenane@hotmail.com>
2024-08-15 01:45:52 +0200

    Worked further on sign up and sign up dialog

---
+++ lib/dialogs/sign_up_dialog.dart
@@ -0,0 +1,162 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class SignUpDialog extends StatefulWidget {
+  const SignUpDialog({
+    Key? key,
+    required this.email,
+    required this.password,
+  }) : super(key: key);
+
+final String email;
+final String password;  
+  @override
+  _SignUpDialogState createState() => _SignUpDialogState();
+}
+
+class _SignUpDialogState extends State<SignUpDialog> {
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  var passwordVisible;
+ 
+
+@override  
+void initState() { 
+    passwordVisible = true;
+super.initState();
+      emailController.text = widget.email;
+    passwordController.text = widget.password;
+    
+}
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        'Set Up Your Account',
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextFormField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: emailController,
+    enabled: false, 
+
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              IgnorePointer(
+              ignoring: true,
+                child: TextFormField(
+                  controller: passwordController,
+                
+                  obscureText: passwordVisible!,
+                 
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                
+suffixIcon: IconButton( 
+
+icon: Icon(
+
+
+    passwordVisible ? Icons.visibility : Icons.visibility_off,
+color: Theme.of(context).primaryColorDark,
+),
+onPressed: () {
+
+setState(() {
+  passwordVisible = !passwordVisible;
+});(() {
+
+
+
+});
+
+},
+
+),
+                ),
+                  
+             
+                
+                
+                  
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                  
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); 
+          },
+          child: Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+      
+          },
+          child: Text('Create your account '),
+        ),
+      ],
+    );
+
+    
+  }
+
+
+}
--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,7 +1,7 @@
 import 'package:dinnerapp/homepage.dart';
 import 'package:flutter/material.dart ';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'dialogs/sign_up_dialog.dart';
 
 
 
@@ -18,7 +18,40 @@ class _signUpState extends State<signUp> {
   final GlobalKey<FormState> _key = GlobalKey<FormState>();
   bool showPassword = false;
 
-  
+  Future<void> _signUp() async {
+    if (_key.currentState!.validate()) {
+      try {
+    
+    
+
+
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          
+          email: emailController.text,
+          password: passwordController.text,
+
+        );
+        if (userCredential.user != null) {
+     showDialog(
+  context: context,
+  builder: (context) {
+    return SignUpDialog(email: emailController.text, password: passwordController.text);
+  },
+);
+        }
+
+        
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          print('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          print('The account already exists for that email.');
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,18 +68,47 @@ class _signUpState extends State<signUp> {
               TextFormField(
                 controller: emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
-             
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 controller: passwordController,
                 decoration: InputDecoration(
-                  
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(showPassword ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },
+                  ),
                 ),
-                
-                
+                obscureText: !showPassword,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
               ),
               ElevatedButton(
-                onPressed: null,
+                onPressed: () {
+
+                  _signUp();
+ showDialog(
+  context: context,
+  builder: (context) {
+    return SignUpDialog(email: emailController.text, password: passwordController.text);
+
+  },
+);
+                },
+              
                 child: const Text('Sign Up'),
               ),
             ],
