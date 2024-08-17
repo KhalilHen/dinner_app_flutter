cd6eb2d9a77727d582ac97bb707f97c5fb575c06
Khalil <khalilhenane@hotmail.com>
2024-08-17 23:00:40 +0200

    Created that user can add now a meal  thought choose meal dialog

--- lib/dialogs/add_meal_to_list.dart
+++ lib/dialogs/add_meal_to_list.dart
@@ -2,6 +2,7 @@ import 'package:dinnerapp/controllers/log_out_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 class AddMealDialog extends StatefulWidget {
   const AddMealDialog({
     Key? key,
@@ -18,113 +19,73 @@ class AddMealDialog extends StatefulWidget {
 
 class _AddMealDialogState extends State<AddMealDialog> {
   final _listNameController = TextEditingController();
-late  String mealName;
-late String mealId;
-String? selectedList;
+  late String mealName;
+  late String mealId;
+  String? selectedList;
+  String? selectedListId;
 
-//TODO Later turn this into a seperate file and call the method from that file
-//TODO And other files where this is used
-    final user = FirebaseAuth.instance.currentUser;
+  final user = FirebaseAuth.instance.currentUser;
   List<DropdownMenuItem<String>> _dropdownItems = [];
 
   @override
-void initState() {
-  
-  super.initState();
+  void initState() {
+    super.initState();
     mealName = widget.name;
     mealId = widget.idMeal;
-        retrieveUsersList();
+    retrieveUsersList();
+  }
 
-}
-
-
- Future<void> retrieveUsersList() async {
-
+  Future<void> retrieveUsersList() async {
     var user = FirebaseAuth.instance.currentUser;
-    var userLinkedList = FirebaseFirestore.instance.collection('list').where('userId', isEqualTo: user!.uid);
+    var userLinkedList = FirebaseFirestore.instance
+        .collection('list')
+        .where('userId', isEqualTo: user!.uid);
 
-var querySnapshot = await userLinkedList.get();
-List<DropdownMenuItem<String>> items = [];
+    var querySnapshot = await userLinkedList.get();
+    List<DropdownMenuItem<String>> items = [];
 
+    for (var queryDocumentSnapshot in querySnapshot.docs) {
+      var data = queryDocumentSnapshot.data();
+      var title = data['title'];
+      var listId = queryDocumentSnapshot.id; // Get the document ID
 
-for (var queryDocumentSnapshot in querySnapshot.docs) {
-
-var data  = queryDocumentSnapshot.data();
-var title = data   ['title'];
-items.add(DropdownMenuItem(
-  child: Text(title),
-  value: title,
-));
-
+      items.add(DropdownMenuItem(
+        child: Text(title), 
+        value: listId, // Store the list ID as the value
+      ));
+    }
 
     setState(() {
       _dropdownItems = items;
     });
-}
+  }
 
-    // print('User List: $userLinkedList');
+  void retrievePickedMeal() {
+    print("mealId: ${widget.idMeal}");
+    print("MealName${widget.name}");
+  }
 
-
-}
-void retrievePickedMeal() {
-
-
-
-print("mealId: ${widget.idMeal}");
-print("MealName${widget.name}");
-
-}
-
-  
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('To which list would you like to add this meal?'),
-
-  
-      
       content: Column(
-        children: 
-        
-        
-        
-        [
-    Text("Meal ID: $mealId"),
-          Text("Meal Name: $mealName"),          
-           Form(
-        
-
-
-          
-          child: DropdownButtonFormField(
-          
-                        items: _dropdownItems,
-
-
-          hint: Text('Select a list'),
-          
-
-    selectedItemBuilder: (BuildContext context) {
-      return _dropdownItems.map<Widget>((item) {
-        return Text(item.value!);
-      }).toList();
-    },
-      onChanged: (String?  newValue) {
-        setState(() { 
-          selectedList = newValue; 
-
-        });
-      },
-
-        
+        mainAxisSize: MainAxisSize.min, // Adjust column to wrap content
+        children: [
+          Text("Meal ID: $mealId"),
+          Text("Meal Name: $mealName"),
+          DropdownButtonFormField<String>(
+            items: _dropdownItems,
+            hint: Text('Select a list'),
+            value: selectedListId,
+            onChanged: (String? newValue) {
+              setState(() {
+                selectedListId = newValue;
+                selectedList = _dropdownItems.firstWhere(
+                    (item) => item.value == newValue).child.toString();
+              });
+            },
           ),
-
-         
-        ),
-       
-          
-          
-        
         ],
       ),
       actions: <Widget>[
@@ -136,30 +97,25 @@ print("MealName${widget.name}");
         ),
         TextButton(
           onPressed: () {
-            if (selectedList != null) {
+            if (selectedListId != null) {
 
 
-
-                //TODO Later turn this into a fetch meal So it fetch every information based on the id of meal
-                //TODO Or just sent the description and everything within this file
-             
-
-     FirebaseFirestore.instance.collection('lists').where(isEqualTo: selectedList.id)({
+              FirebaseFirestore.instance.collection('list').doc(selectedListId).collection('mealItem').add({
                 'mealId': mealId,
                 'mealName': mealName,
               });
-              print('Selected List: $selectedList');
+
+              print('Selected List Name: $selectedList');
+              print('Selected List ID: $selectedListId');
               print('Meal ID: $mealId');
               print('Meal Name: $mealName');
             }
 
             // Add logic to handle adding the meal to the list here
-    
           },
           child: Text('Add'),
         ),
       ],
     );
   }
-
 }
